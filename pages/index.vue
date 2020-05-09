1e6343782a83988812a97fe981fb920619e6fa6a
Masafumi Yokoyama <myokoym@gmail.com>
2020-05-10 03:59:17 +0900

    index: design and add button to copy to clioboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="container">
-    <h1>
-      <img
-        class="title-logo"
-        src="@/assets/images/logo.png"
-        alt="将棋ウェブルーム"
-      >
-    </h1>
-    <div>
-      <label for="roomId">ルームID（空欄の場合は自動生成）:</label>
-      <input type="text" id="roomId" v-model="roomId">
-      <button @click="enterRoom">ルーム入室</button>
+  <div class="container d-flex flex-row justify-content-center">
+    <div class="d-flex flex-column">
+      <div>
+        <h1>
+          <img
+            class="title-logo"
+            src="@/assets/images/logo.png"
+            alt="将棋ウェブルーム"
+          >
+        </h1>
+      </div>
+      <div class="row">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="roomId">ルームID</span>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            aria-describedby="roomId"
+            v-model="roomId"
+          >
+          <button
+            type="button"
+            class="btn btn-secondary btn-sm"
+            @click="generateId"
+          >自動生成</button>
+        </div>
+      </div>
+      <div class="row">
+        <button
+          type="button"
+          class="btn btn-success btn-sm"
+          v-clipboard:copy="this.roomUrl"
+          v-bind:disabled="!this.roomId"
+        >ルームのURLをコピー（共有用）</button>
+      </div>
+      <div class="row">
+        <button
+          type="button"
+          class="btn btn-primary btn-lg"
+          @click="enterRoom"
+          v-bind:disabled="!this.roomId"
+        >入室</button>
+      </div>
     </div>
   </div>
 </template>
@@ -18,20 +51,37 @@
 <script>
 import Vue from "vue"
 import cryptoRandomString from "crypto-random-string"
+import VueClipboard from "vue-clipboard2"
+Vue.use(VueClipboard)
 
 export default Vue.extend({
   data() {
     return {
+      origin: "",
       roomId: "",
     }
   },
+  mounted() {
+    this.origin = location.origin
+  },
+  computed: {
+    roomPath: function() {
+      return "/rooms/" + this.roomId
+    },
+    roomUrl: function() {
+      return this.origin + this.roomPath
+    },
+  },
   methods: {
+    generateId() {
+      this.roomId = cryptoRandomString({length: 16})
+    },
     enterRoom() {
       if (!this.roomId) {
-        this.roomId = cryptoRandomString({length: 8})
+        return
       }
-      this.$router.push("/rooms/" + this.roomId)
-    }
+      this.$router.push(this.roomPath)
+    },
   }
 })
 </script>
@@ -42,5 +92,12 @@ body {
 img.title-logo {
   max-width: 100%;
   height: auto;
+}
+.row {
+  margin: 4%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 </style>
