6ba922cc2c22d990e2f53f7991bce719caf44927
Masafumi Yokoyama <myokoym@gmail.com>
2020-05-11 22:37:59 +0900

    try to update board when reverse in iOS

--- store/index.js
+++ store/index.js
@@ -15,6 +15,7 @@ const webSocketPlugin = (store) => {
     if (mutation.type === "sfen/setText" ||
         mutation.type === "sfen/prevHistory" ||
         mutation.type === "sfen/nextHistory" ||
+        mutation.type === "sfen/reverse" ||
         mutation.type === "sfen/buildSfen") {
       // debug: console.log("send")
       //console.log(state.sfen)
