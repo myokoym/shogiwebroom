3494908a184d92903c002afb7000b0528f4c0ea0
Masafumi Yokoyama <myokoym@gmail.com>
2020-05-13 02:17:44 +0900

    try to add Twitter card

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,12 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'リアルタイムで同期する将棋盤を複数人が自由に操作できるWebアプリ。自由対局や感想戦、研究会などに。ドラッグ＆ドロップ対応。スマートフォン、タブレット対応（レスポンシブ）。SFEN形式（USI）入出力対応。' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@shogiwebroom' },
+      { property: 'og:url', content: 'https://shogiwebroom.herokuapp.com/' },
+      { property: 'og:title', content: '将棋ウェブルーム' },
+      { property: 'og:description', content: 'ブラウザで気軽に将棋盤を囲めるWebアプリ。ログイン不要、URL共有で何人でも入室可。リアルタイム同期、ドラッグ＆ドロップ対応、スマートフォン・タブレット対応、簡易チャット機能などが特徴。研究会などにお使いください。' },
+      { property: 'og:image', content: 'https://shogiwebroom.herokuapp.com/logo.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
